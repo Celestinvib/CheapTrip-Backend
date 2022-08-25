24c2689dda7f5b637ac0263b73fde3564ea27d0d
pbesora <pau_besora@hotmail.com>
2022-08-25 09:58:19 +0200

    Service interface city, feature and role created

--- CheapTrip/src/main/java/com/cheaptrip/demo/service/ICityService.java
+++ CheapTrip/src/main/java/com/cheaptrip/demo/service/ICityService.java
@@ -1,5 +1,19 @@
 package com.cheaptrip.demo.service;
 
-public interface ICityService {
+import java.util.List;
 
+import com.cheaptrip.demo.dto.City;
+
+public interface ICityService {
+	
+	//CRUD
+	public List<City> listCities(); 	//List
+
+	public City saveCity(City city);	//Save
+
+	public City cityByID(int id); 		//Read
+
+	public City updateCity(City city);	//Update
+
+	public void deleteCity(int id);		//Delete
 }
--- CheapTrip/src/main/java/com/cheaptrip/demo/service/IFeatureService.java
+++ CheapTrip/src/main/java/com/cheaptrip/demo/service/IFeatureService.java
@@ -1,5 +1,19 @@
 package com.cheaptrip.demo.service;
 
-public interface IFeatureService {
+import java.util.List;
 
+import com.cheaptrip.demo.dto.Feature;
+
+public interface IFeatureService {
+	
+	//CRUD
+	public List<Feature> listFeatures(); 			//List
+
+	public Feature saveFeature(Feature feature);	//Save
+
+	public Feature featureByID(int id); 			//Read
+
+	public Feature updateFeature(Feature feature);	//Update
+
+	public void deleteFeature(int id);				//Delete
 }
