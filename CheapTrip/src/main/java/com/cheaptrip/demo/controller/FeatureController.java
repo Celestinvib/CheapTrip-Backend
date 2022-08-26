bae9ca5f4df8ee31ce1f1b5611a8bb84fb53d51e
pbesora <pau_besora@hotmail.com>
2022-08-26 08:39:53 +0200

    Controller city, feature and role

--- CheapTrip/src/main/java/com/cheaptrip/demo/controller/FeatureController.java
+++ CheapTrip/src/main/java/com/cheaptrip/demo/controller/FeatureController.java
@@ -1,5 +1,69 @@
 package com.cheaptrip.demo.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cheaptrip.demo.dto.Feature;
+import com.cheaptrip.demo.service.FeatureServiceImpl;
+
+@RestController
 public class FeatureController {
 
+	@Autowired
+	FeatureServiceImpl featureServiceImpl;
+	
+	@GetMapping("/features")
+	public List<Feature> listFeatures(){
+		
+		return featureServiceImpl.listFeatures();
+	}
+	
+	@PostMapping("/features")
+	public Feature saveFeature(@RequestBody Feature feature) {
+		
+		return featureServiceImpl.saveFeature(feature);
+	}
+	
+	@GetMapping("/features/{id}")
+	public Feature featureByID(@PathVariable(name="id") Long id) {
+		
+		Feature feature_byid= new Feature();
+		
+		feature_byid= featureServiceImpl.featureByID(id);
+				
+		return feature_byid;
+	}
+	
+	@PutMapping("/features/{id}")
+	public Feature updateFeature(@PathVariable(name="id")Long id,@RequestBody Feature feature) {
+		
+		Feature featureSelected= new Feature();
+		Feature featureUpdated = new Feature();
+		
+		featureSelected = featureServiceImpl.featureByID(id);
+		
+		featureSelected.setName(feature.getName());
+	
+		featureUpdated = featureServiceImpl.updateFeature(featureSelected);
+				
+		return featureUpdated;
+	}
+	
+	@DeleteMapping("/features/{id}")
+	public void changeStatusFeature(@PathVariable(name="id")Long id) {
+		featureServiceImpl.deleteFeature(id);
+	}	
+	
+	@DeleteMapping("/features/{id}")
+	public void deleteFeature(@PathVariable(name="id")Long id) {
+		featureServiceImpl.deleteFeature(id);
+	}	
 }
--- CheapTrip/src/main/java/com/cheaptrip/demo/controller/RoleController.java
+++ CheapTrip/src/main/java/com/cheaptrip/demo/controller/RoleController.java
@@ -1,5 +1,69 @@
 package com.cheaptrip.demo.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cheaptrip.demo.dto.Role;
+import com.cheaptrip.demo.service.RoleServiceImpl;
+
+@RestController
 public class RoleController {
 
+	@Autowired
+	RoleServiceImpl roleServiceImpl;
+	
+	@GetMapping("/roles")
+	public List<Role> listRoles(){
+		
+		return roleServiceImpl.listRoles();
+	}
+	
+	@PostMapping("/roles")
+	public Role saveRole(@RequestBody Role role) {
+		
+		return roleServiceImpl.saveRole(role);
+	}
+	
+	@GetMapping("/roles/{id}")
+	public Role roleByID(@PathVariable(name="id") Long id) {
+		
+		Role role_byid= new Role();
+		
+		role_byid= roleServiceImpl.roleByID(id);
+				
+		return role_byid;
+	}
+	
+	@PutMapping("/roles/{id}")
+	public Role updateRole(@PathVariable(name="id")Long id,@RequestBody Role role) {
+		
+		Role roleSelected= new Role();
+		Role roleUpdated = new Role();
+		
+		roleSelected = roleServiceImpl.roleByID(id);
+		
+		roleSelected.setName(role.getName());
+	
+		roleUpdated = roleServiceImpl.updateRole(roleSelected);
+				
+		return roleUpdated;
+	}
+	
+	@DeleteMapping("/roles/{id}")
+	public void changeStatusRole(@PathVariable(name="id")Long id) {
+		roleServiceImpl.deleteRole(id);
+	}	
+	
+	@DeleteMapping("/roles/{id}")
+	public void deleteRole(@PathVariable(name="id")Long id) {
+		roleServiceImpl.deleteRole(id);
+	}	
 }
