2a45dfc5f647d502b36d687e2fb3e0dc5337a98a
Bolgvs <106079820+Bolgvs@users.noreply.github.com>
2022-08-26 11:27:52 +0200

    Update DDL.sql

--- DbDesign&Implementation/DDL.sql
+++ DbDesign&Implementation/DDL.sql
@@ -21,7 +21,7 @@ insert into Users (name, surnames, email, password, phone_number, birth_date, cr
     ('Ylenia', 'Almeria Ballesteros', 'vallesT@gmail.com', 'yosiganeaBnet23/', '642057952', '1978-01-16', '2022-05*10', 1, 11),
     ('Pedro', 'De la Rosa Lobato', 'laF1yAlonso@gmail.com', 'fernandoelnano02&', '456789123', '2001-01-23', '2021-04-22', 1, 21);
 
-insert into Accomodations (name, address, category, latitude, longitude, rating, city_id, status) values
+insert into Accommodations (name, address, category, latitude, longitude, rating, city_id, status) values
 	('Pestana CR7', 'Rua do Comercio', '4', '38.70919', '-9.13596', '8.8', 1, 1),
     ('Chiado Luxury', 'Rua das Flores', '5', '38.70876', '-9.14357', '10', 1, 1),
     ('Uppelink', 'Sint-Michielsplein Strasse', '2', '51.05393', '3.71986', '8.3', 11, 1),
@@ -33,16 +33,16 @@ insert into Flights (origin, destination, departure_date, arrival_date, status) 
     (41, 11, '2022-10-03', '2022-10-06', 1),
     (31, 21, '2022-10-22', '2022-10-24', 1);
     
-insert into Bargains (title, image, price, description, expiration_date, outbound_flight, return_flight, accomodation_id, status) values
+insert into Bargains (title, image, price, description, expiration_date, outbound_flight, return_flight, accommodation_id, status) values
 	('Estancia por las Ruas Portuguesas', 'img', '150', 'una descripcion', '2022-09-27', 1, 1, 1, 1),
     ('Gante y su magia', 'img', '200', 'una descripcion', '2022-09-28', 11, 11, 1, 1),
     ('Tallin, vida de castillos', 'img', '300', 'una descripcion', '2022-09-29', 21, 21, 1, 1);
 
-insert into Accomodations_Features (accomodation_id, feature_id) values
+insert into Accommodations_Features (accommodation_id, feature_id) values
 	(1, 1),
     (11, 11),
     (21, 21);
     
 insert into Bargains_Users (bargain_id, user_id, booked, bookmarked) values
-	(1, 101, 1, 1),
-    (21, 111, 1, 1);
+	(1, 11, 1, 1),
+    (21, 21, 1, 1);
