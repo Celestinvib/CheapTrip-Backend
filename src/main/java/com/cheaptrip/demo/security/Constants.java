b37f17553bd5c0e7177acb134987966dbbabf139
Celestinvib <celestinvib@gmail.com>
2022-09-02 14:10:47 +0200

    Constants file added

---
+++ src/main/java/com/cheaptrip/demo/security/Constants.java
@@ -0,0 +1,17 @@
+package com.cheaptrip.demo.security;
+
+public class Constants {
+
+    // Spring Security
+
+    public static final String LOGIN_URL = "";
+    public static final String HEADER_AUTHORIZACION_KEY = "";
+    public static final String TOKEN_BEARER_PREFIX = "";
+
+    // JWT
+
+    public static final String ISSUER_INFO = "";
+    public static final String SUPER_SECRET_KEY = "";
+    public static final long TOKEN_EXPIRATION_TIME = 0; 
+
+}
