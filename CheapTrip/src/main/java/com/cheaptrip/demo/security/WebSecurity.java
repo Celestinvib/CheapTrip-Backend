1a07d94b4a54c649bccbf940baffe59524f3784c
Celestinvib <celestinvib@gmail.com>
2022-09-09 12:29:56 +0200

    Security code refactored

--- CheapTrip/src/main/java/com/cheaptrip/demo/security/WebSecurity.java
+++ CheapTrip/src/main/java/com/cheaptrip/demo/security/WebSecurity.java
@@ -45,8 +45,7 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
 			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
 			.cors().and()
 			.csrf().disable()
-			.authorizeRequests().antMatchers(HttpMethod.POST, LOGIN_URL).permitAll()
-			.antMatchers(HttpMethod.POST, REGISTER_URL).permitAll()
+			.authorizeRequests().antMatchers(HttpMethod.POST, LOGIN_URL,REGISTER_URL).permitAll()
 			.anyRequest().authenticated().and()
 				.addFilter(new JWTAuthenticationFilter(authenticationManager()))
 				.addFilter(new JWTAuthorizationFilter(authenticationManager()));
