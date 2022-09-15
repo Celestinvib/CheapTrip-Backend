c5455caa5bfd962e43f85d39c548571ed3345079
pbesora <pau_besora@hotmail.com>
2022-09-16 00:32:45 +0200

    Constants moved to Config Vars

--- src/main/java/com/cheaptrip/demo/security/Constants.java
+++ src/main/java/com/cheaptrip/demo/security/Constants.java
@@ -3,14 +3,14 @@ package com.cheaptrip.demo.security;
 public class Constants {
 
     // Spring Security
-    public static final String REGISTER_URL = "/registrar";
-    public static final String LOGIN_URL = "/login";
-    public static final String HEADER_AUTHORIZACION_KEY = "Authorization";
-    public static final String TOKEN_BEARER_PREFIX = "Bearer ";
+    public static final String REGISTER_URL = System.getenv("SPRING_REGISTER_URL");;
+    public static final String LOGIN_URL = System.getenv("SPRING_LOGIN_URL");;
+    public static final String HEADER_AUTHORIZACION_KEY = System.getenv("SPRING_HEADER_AUTHORIZACION_KEY");;
+    public static final String TOKEN_BEARER_PREFIX = System.getenv("SPRING_TOKEN_BEARER_PREFIX");;
 
     // JWT
 
-    public static final String ISSUER_INFO = "Cheaptrip Admin";
-    public static final String SUPER_SECRET_KEY = "1234";
+    public static final String ISSUER_INFO = System.getenv("JWT_ISSUER_INFO");
+    public static final String SUPER_SECRET_KEY = System.getenv("JWT_SUPER_SECRET_KEY");
     public static final long TOKEN_EXPIRATION_TIME = 7_200_000; // 2 hours
 }
