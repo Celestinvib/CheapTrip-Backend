6d47a2e9e2fb8e92d622bf808131025c54eebdfe
Celestinvib <celestinvib@gmail.com>
2022-09-26 21:57:17 +0200

    Users can unbook/unbookmark(only theirs bargains)

--- CheapTrip/src/main/java/com/cheaptrip/demo/controller/BargainsAccountsController.java
+++ CheapTrip/src/main/java/com/cheaptrip/demo/controller/BargainsAccountsController.java
@@ -1,10 +1,15 @@
 package com.cheaptrip.demo.controller;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,7 +18,11 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.cheaptrip.demo.dao.IAccountDAO;
+import com.cheaptrip.demo.dto.Account;
 import com.cheaptrip.demo.dto.BargainsAccounts;
+import com.cheaptrip.demo.dto.Role;
+import com.cheaptrip.demo.service.AccountServiceImpl;
 import com.cheaptrip.demo.service.BargainsAccountsServiceImpl;
 
 @RestController
@@ -22,6 +31,11 @@ public class BargainsAccountsController {
 	@Autowired
 	BargainsAccountsServiceImpl bargainsAccountsServiceImpl;
 	
+	AccountServiceImpl accountServiceImpl;
+	
+	private IAccountDAO iUserDAO;
+
+
 	
 	@GetMapping("/reservas")
 	public List<BargainsAccounts> listBargainsAccountsBooked() {
@@ -108,40 +122,99 @@ public class BargainsAccountsController {
 		return bargainAccountsUpdated;
 	}
 	
-	@PreAuthorize("hasRole('ADMIN')")
 	@PutMapping("/chollo-reservado/cambiar-estado/{id}")
-	public BargainsAccounts changeStatusBooked(@PathVariable(name="id")Long id) {
+	public ResponseEntity changeStatusBooked(@PathVariable(name="id")Long id) {
 		
 		BargainsAccounts bargainsAccounts = bargainsAccountsXID(id);
 		BargainsAccounts bargainsAccountsUpdated = new BargainsAccounts();
 
+		Account accountSelected = bargainsAccounts.getAccount(); 
 		
-		if(bargainsAccounts.getBooked() == 0) { 
-			bargainsAccounts.setBooked(1);
+		//If the account is trying to change the status is the same as the one who have the rel allows it
+		if (accountSelected == null) {
+			
+			return new ResponseEntity<Object>("No account found with that id...",HttpStatus.INTERNAL_SERVER_ERROR);
+		
 		}else {
-			bargainsAccounts.setBooked(0);	
+			
+			String accountSelectedEmail = accountSelected.getEmail();
+			String loggedAccountEmail = ((Account) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getEmail();
+
+			Set<Role> accountRoles = ((Account) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getRoles();
+
+			HashSet<String> userRolesArray = new HashSet<String>(accountRoles.size());
+
+			// Get all the roles 
+			for (Role role : accountRoles) {
+				userRolesArray.add(role.toString());
+			}
+ 
+			//If the account that is trying to delete is the same that is making the request (account logged) or is an admin account allows to change it
+			if (accountSelectedEmail.equals(loggedAccountEmail) || userRolesArray.contains("ROLE_ADMIN") == true) {  
+
+				if(bargainsAccounts.getBooked() == 0) { 
+					bargainsAccounts.setBooked(1);
+				}else {
+					bargainsAccounts.setBooked(0);	
+				}
+				
+				bargainsAccountsUpdated = bargainsAccountsServiceImpl.updateBargainsAccounts(bargainsAccounts);
+				
+					return new ResponseEntity<Object>(bargainsAccountsUpdated, HttpStatus.OK);
+			}else {
+				return new ResponseEntity<Object>("You have no permission to book/unbook for this account",HttpStatus.UNAUTHORIZED);
+			}
 		}
 		
-		bargainsAccountsUpdated = bargainsAccountsServiceImpl.updateBargainsAccounts(bargainsAccounts);
-		return bargainsAccountsUpdated;
+
+		
+
 	}
-	
-	@PreAuthorize("hasRole('ADMIN')")
 	@PutMapping("/chollo-favorito/cambiar-estado/{id}")
-	public BargainsAccounts changeStatusBookmarked(@PathVariable(name="id")Long id) {
+	public ResponseEntity changeStatusBookmarked(@PathVariable(name="id")Long id) {
+			
 		
 		BargainsAccounts bargainsAccounts = bargainsAccountsXID(id);
 		BargainsAccounts bargainsAccountsUpdated = new BargainsAccounts();
-
 		
-		if(bargainsAccounts.getBookmarked() == 0) { 
-			bargainsAccounts.setBookmarked(1);
+		Account accountSelected = bargainsAccounts.getAccount(); 
+		
+		//If the account is trying to change the status is the same as the one who have the rel allows it
+		if (accountSelected == null) {
+			
+			return new ResponseEntity<Object>("No account found with that id...",HttpStatus.INTERNAL_SERVER_ERROR);
+		
 		}else {
-			bargainsAccounts.setBookmarked(0);	
+			
+			String accountSelectedEmail = accountSelected.getEmail();
+			String loggedAccountEmail = ((Account) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getEmail();
+
+			Set<Role> accountRoles = ((Account) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getRoles();
+
+			HashSet<String> userRolesArray = new HashSet<String>(accountRoles.size());
+
+			// Get all the roles 
+			for (Role role : accountRoles) {
+				userRolesArray.add(role.toString());
+			}
+ 
+			//If the account that is trying to delete is the same that is making the request (account logged) or is an admin account allows to change it
+			if (accountSelectedEmail.equals(loggedAccountEmail) || userRolesArray.contains("ROLE_ADMIN") == true) {  
+
+				if(bargainsAccounts.getBookmarked() == 0) { 
+					bargainsAccounts.setBookmarked(1);
+				}else {
+					bargainsAccounts.setBookmarked(0);	
+				}
+				
+				bargainsAccountsUpdated = bargainsAccountsServiceImpl.updateBargainsAccounts(bargainsAccounts);
+				
+					return new ResponseEntity<Object>(bargainsAccountsUpdated, HttpStatus.OK);
+			}else {
+				return new ResponseEntity<Object>("You have no permission to bookmark/unbookmark for this account",HttpStatus.UNAUTHORIZED);
+			}
 		}
-		
-		bargainsAccountsUpdated = bargainsAccountsServiceImpl.updateBargainsAccounts(bargainsAccounts);
-		return bargainsAccountsUpdated;
+
 	}
 	
 	@PreAuthorize("hasRole('ADMIN')")
